804b2bb666db85610562fbc44d1623e49cde1549
Mukesh Yadav <mukesh.yadav@mindtickle.com>
2022-11-10 08:11:12 +0530

    package update

--- Package.swift
+++ Package.swift
@@ -1,22 +1,23 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Reachability",
     platforms: [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .tvOS(.v9),
+        .iOS(.v11)
     ],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Reachability",
-            targets: ["Reachability"]),
+            targets: ["Reachability"])
     ],
     dependencies: [],
     targets: [
-        .target(
-            name: "Reachability"),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "Reachability")
     ]
 )
